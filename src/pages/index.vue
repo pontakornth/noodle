b368c14160257a2e907402237641b22c1e3ffca0
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-02-01 19:35:37 +0700

    feat: Add empty box

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const guess = ref('')
 </script>
 
 <template>
@@ -11,15 +12,18 @@
     </p>
 
     <div class="w-full p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 my-4 mx-auto grid grid-cols-4 grid-rows-4 gap-4">
-      <div class="border-green-400 flex rounded border aspect-square justify-center items-center font-bold text-lg" />
-      <div class="border-green-400 flex rounded border aspect-square justify-center items-center font-bold text-lg" />
-      <div class="border-green-400 flex rounded border aspect-square justify-center items-center font-bold text-lg" />
-      <div class="border-green-400 flex rounded border aspect-square justify-center items-center font-bold text-lg" />
+      <div
+        v-for="g, idx in guess.split('').slice(0, 4)" :key="idx"
+        data-testid="box"
+        class="border-green-400 flex rounded border aspect-square justify-center items-center font-bold text-lg"
+      >
+        {{ g }}
+      </div>
     </div>
 
     <div class="flex flex-col items-center">
       <label for="guess" class="text-green-600 dark:text-green-50 mb-4">Input your guess here</label>
-      <input id="guess" class="text-black dark:(text-green-50 bg-transparent) px-3 py-2 border border-green-500" type="number" name="guess">
+      <input id="guess" v-model="guess" class="text-black dark:(text-green-50 bg-transparent) px-3 py-2 border border-green-500" type="text" name="guess">
     </div>
   </div>
 </template>
